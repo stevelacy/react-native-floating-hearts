4cf73ab9542138fd98ddd5b1c57b70921efa9ddf
Steve Lacy <me@slacy.me>
2018-09-09 14:31:07 -0700

    Upgrade to support react-native 0.56+

--- FloatingHearts.js
+++ FloatingHearts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { View, Animated, StyleSheet } from 'react-native'
+import { View, Animated, StyleSheet, ViewPropTypes } from 'react-native'
 
 import HeartShape from './HeartShape'
 
@@ -70,7 +70,7 @@ class FloatingHearts extends Component {
 }
 
 FloatingHearts.propTypes = {
-  style: View.propTypes.style,
+  style: ViewPropTypes.style,
   count: PropTypes.number,
   color: PropTypes.string,
   renderCustomShape: PropTypes.func,
@@ -174,7 +174,7 @@ class AnimatedShape extends Component {
 AnimatedShape.propTypes = {
   height: PropTypes.number.isRequired,
   onComplete: PropTypes.func.isRequired,
-  style: View.propTypes.style,
+  style: ViewPropTypes.style,
   children: PropTypes.node.isRequired,
 }
 
